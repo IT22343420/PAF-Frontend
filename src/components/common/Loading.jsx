96d5d5ba1057beca77131ad4dd40d785dc272508
yuwanirashipaba <it22343352@my.sliit.lk>
2025-05-06 20:24:59 +0530

    moved styles to css folder

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -1,30 +1,9 @@
 import React from 'react';
-
+import './css/Loading.css';
 const Loading = () => {
     return (
-        <div style={{
-            position: 'fixed',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            zIndex: 1000
-        }}>
-            <div style={{
-                width: '50px',
-                height: '50px',
-                border: '5px solid #f3f3f3',
-                borderTop: '5px solid #3498db',
-                borderRadius: '50%',
-                animation: 'spin 1s linear infinite'
-            }} />
-            <style>
-                {`
-                    @keyframes spin {
-                        0% { transform: rotate(0deg); }
-                        100% { transform: rotate(360deg); }
-                    }
-                `}
-            </style>
+        <div className="loading-container">
+            <div className="loading-spinner" />
         </div>
     );
 };
